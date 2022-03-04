ed2ee52c55e194a26f8a150f8f7e81bb28c8ffca
Kevin Real <kevinreal8994@gmail.com>
2022-03-04 13:17:37 +0100

    create clasship with map and fetch 4 pages of ships

--- src/main.js
+++ src/main.js
@@ -9,3 +9,17 @@ export const countStartShip = async () => {
   const { count } = await starShipsApi();
   return count;
 };
+
+const classShips = async () => {
+  //const { results } = await starShipsApi;
+
+  const pages = [1, 2, 3, 4];
+  const shipsPromsesas = pages.map(async (page) => {
+    const response = await fetch(`${process.env.REACT_APP_PAGE}${page}`);
+    const { results } = await response.json();
+    return results;
+  });
+
+  const ships = await Promise.all(shipsPromsesas);
+  return ships;
+};
